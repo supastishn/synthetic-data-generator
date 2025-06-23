b1607fdbe87ff6f2381ceac1022a3215120796d3
supastishn <omareeto2012@hotmail.com>
2025-06-23 15:00:15 +0000

    feat: add main.py

---
+++ main.py
@@ -0,0 +1,63 @@
+from litellm import completion
+import os
+
+# Check for necesarry lenvironment variable
+
+if "PROMPTGEN_MODEL" not in os.environ:
+    print("Error: The environment variable PROMOTGEN_MODEL is not set.")
+    exit(1)
+
+if "ANSWERGEN_MODEL" not in os.environ:
+    print("Error: The environment variable ANSWERGEN_MODEL is not set.")
+    exit(1)
+
+if "TEMPERATURE" not in os.environ:
+    print("Warning: The environment variable TEMPERATURE is not set. Using default value of 0.7.")
+    temp = 0.7
+
+promptgen_model = os.environ["PROMPTGEN_MODEL"]
+answergen_model = os.environ["ANSWERGEN_MODEL"]
+
+
+topic = input("Please enter 1 or more topics, separated by a comma: ")
+
+topics = [topic.strip() for topic in topic.split(",")]
+
+amount = input("How many prompt/answer combination do you want to create? Please enter either one number, or the same amount of numbers as topics, sepatated by a comma: ")
+
+amounts = [int(a.strip()) for a in amount.split(",")]
+
+if len(amounts) != 1 and len(amounts) != len(topics):
+    print("Error: The amount of numbers must either be 1 or the same amount as topics.")
+    exit(1)
+
+multiprompt_input = ""
+
+while multiprompt_input not in ["y", "yes", "n", "no", ""]:
+    multiprompt_input = input("Do you want to use multiprompt generation? This saves requests by generating 10 prompts per request. (Y/n): ").strip().lower()
+    if multiprompt_input not in ["y", "yes", "n", "no", ""]:
+        print("Invalid input. Please enter 'y' or 'n'.")
+
+multiprompt = False
+
+if multiprompt_input == "y":
+    multiprompt = True
+
+
+if len(amounts) == 1:
+    amounts = [amounts[0]] * len(topics)
+
+prompts = []
+
+def generate_prompt(topic, amount):
+    print(f"Generating {amount} prompts for topic: {topic}")
+    response = completion(
+    model=promptgen_model,
+     messages=[
+            { "content": "You are an expert prompt maker.", "role": "system" }
+            { "content": f"Generate {amount} prompts for this topic: {topic}","role": "user"}
+        ],
+    temperature=temp,
+    )
+
+
